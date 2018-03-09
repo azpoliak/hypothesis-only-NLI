694a73755e19e74b5e12b70c6fd105af658553a3
Aparajita Haldar <ahaldar@a13.clsp.jhu.edu>
2018-03-08 21:04:41 -0500

    Sentence lengths

--- src/util/plot-vocab.py
+++ src/util/plot-vocab.py
@@ -13,7 +13,8 @@ def get_args():
 
   parser.add_argument('--preds', type=int, help="Determine whether tosplit based on predictions or not")
   parser.add_argument('--vocab_thresh', type=int, default=25, help="Only include words that appear this many times")
-  parser.add_argument('--percent_keep', type=int, default=40, help="Only keep words high percentages higher than this")
+  parser.add_argument('--len_thresh', type=int, default=5, help="Only include sentences that are this long")
+  parser.add_argument('--percent_keep', type=int, default=40, help="Only keep high percentages higher than this")
   parser.add_argument('--top_k', type=int, default=100, help="Top k examples to keep. k=100")
 
   args = parser.parse_args()
@@ -108,6 +109,59 @@ def get_vocab_counts(data, use_preds, threshold, threshold_percent):
   return df, all_vocab
 
 
+def get_sent_lens(data, use_preds, threshold, threshold_percent):
+ 
+  all_lens = {}
+  threshold_percent = threshold_percent / 100.0
+  print threshold_percent
+  sent_lens = {'correct': {}, 'wrong': {}}
+  
+  for key in data:
+    for lbl in data[key]:
+      if lbl not in sent_lens[key]:
+        sent_lens[key][lbl] = {}
+      for sent in data[key][lbl]:
+        l = len(sent.split())
+        if l not in sent_lens[key][lbl]:
+          sent_lens[key][lbl][l] = 0
+        sent_lens[key][lbl][l] += 1
+        if l not in all_lens:
+          all_lens[l] = 0
+        all_lens[l] += 1
+
+  df = None
+  if not use_preds:
+    df = pd.DataFrame(columns=['gold-lbl', 'len', 'count', 'total'])
+    for l in all_lens:
+      for lbl in set(sent_lens['correct'].keys() + sent_lens['wrong'].keys()):
+        correct, wrong = 0, 0
+        if lbl in sent_lens['correct']:
+          if l in sent_lens['correct'][lbl]:
+            correct = sent_lens['correct'][lbl][l]
+        if lbl in sent_lens['wrong']:
+          if l in sent_lens['wrong'][lbl]:
+            wrong = sent_lens['wrong'][lbl][l]
+        count = (correct + wrong) / float(all_lens[l])
+        if all_lens[l] >= threshold and count >= threshold_percent:
+          df = df.append({'gold-lbl': lbl, 'len': l, 'count': count, 'total': all_lens[l]}, ignore_index = True)
+
+    return df, all_lens
+
+  df = pd.DataFrame(columns=['gold-lbl', 'correct', 'len', 'count'])
+  for correct in sent_lens:
+    for lbl in sent_lens[correct]:
+      for l in sent_lens[correct][lbl]:
+        df = df.append({'gold-lbl': lbl, 'correct': correct, 'len': l, 'count': sent_lens[correct][lbl][l]}, ignore_index=True)
+
+  for correct in sent_lens:
+    for lbl in sent_lens[correct]:
+      for l in all_lens.difference(sent_lens[correct][lbl].keys()):
+        df = df.append({'gold-lbl': lbl, 'correct': correct, 'len': l, 'count': 0}, ignore_index=True)
+
+  return df, all_lens
+
+
+
 def main():
   
   args = get_args()
@@ -118,9 +172,18 @@ def main():
     df = df.sort_values(by=['count', 'total'], ascending=False)
     df = df.head(args.top_k)
     html = df.to_html()
-    f_out = open("%s_top%d_mincount%d_minpercent%d.html" % (args.data_src, args.top_k, args.vocab_thresh, args.percent_keep), "wb")
+    f_out = open("%s_top%d_mincount%d_minpercent%d_words.html" % (args.data_src, args.top_k, args.vocab_thresh, args.percent_keep), "wb")
     f_out.write(html)
     f_out.close() 
+
+    df2, lengths = get_sent_lens(data, args.preds, args.len_thresh, args.percent_keep)
+    df2 = df2.sort_values(by=['count', 'total'], ascending=False)
+    df2 = df2.head(args.top_k)
+    html2 = df2.to_html()
+    f_out2 = open("%s_top%d_mincount%d_minpercent%d_lens.html" % (args.data_src, args.top_k, args.vocab_thresh, args.percent_keep), "wb")
+    f_out2.write(html)
+    f_out2.close()
+
     df.to_pickle("%s_tokens_count.pkl" % (args.data_src)) 
 
 
